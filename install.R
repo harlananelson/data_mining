7311d83c6c18668890938e74bef8ce0a7ad494be
harlananelson <harlananelson@gmail.com>
2018-01-29 17:25:28 -0500

    added tables

--- install.R
+++ install.R
@@ -1,5 +1,8 @@
+# This is a list of packages I have installed
+install.packages('ca')
 install.packages('devtools')
 install.packages('skimr')
 install.packages('DT')
 install.packages('tidyverse')
 install.packages('psych')
+install.packages('gmodels')
--- library.R
+++ library.R
@@ -1,3 +1,9 @@
+# This is a list of libraries needed for the analysis.
+# I keep this list so I can call it at the top of any notebook.
+
+library(ca)
+library(gmodels)
+library(vcd)
 library(devtools)
 library(DT)
 library(tidyverse)
@@ -6,3 +12,4 @@ library(ggplot2)
 library(magrittr)
 library(modelr)
 library(broom)
+library(Hmisc)
